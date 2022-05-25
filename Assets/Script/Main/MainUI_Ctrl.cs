eb07e1c2a3ed80e0bab2f63cf4f7b128104b70aa
ChoiSou0 <95004780+ChoiSou0@users.noreply.github.com>
2022-05-25 20:34:41 +0900

    5/25 작업

--- Assets/Script/Main/MainUI_Ctrl.cs
+++ Assets/Script/Main/MainUI_Ctrl.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class MainUI_Ctrl : MonoBehaviour
 {
+    [SerializeField] GameObject Select_Pnl;
     
     // Start is called before the first frame update
     void Start()
@@ -17,4 +18,46 @@ public class MainUI_Ctrl : MonoBehaviour
     {
         
     }
+
+    // 버튼 클릭
+    #region
+    // 옵션 버튼 클릭
+    public void OnClickOption()
+    {
+
+    }
+
+    // 주제 선택 버튼
+    #region
+    public void OnClickAni()
+    {
+        Select_Pnl.SetActive(true);
+    }
+
+    public void OnClickFruit()
+    {
+        Select_Pnl.SetActive(true);
+    }
+
+    public void OnClickVege()
+    {
+        Select_Pnl.SetActive(true);
+    }
+
+    public void OnClickVehicle()
+    {
+        Select_Pnl.SetActive(true);
+    }
+    #endregion
+
+    // 모드 선택 버튼
+
+
+    public void OnClickCancel()
+    {
+        Select_Pnl.SetActive(false);
+    }
+
+    #endregion
+
 }
