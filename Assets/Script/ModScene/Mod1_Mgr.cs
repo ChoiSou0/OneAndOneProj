17b78803b02f84571979d2b77b06d5b30e3d2a56
ChoiSou0 <95004780+ChoiSou0@users.noreply.github.com>
2022-06-16 13:15:37 +0900

    6/16 작업1

--- Assets/Script/ModScene/Mod1_Mgr.cs
+++ Assets/Script/ModScene/Mod1_Mgr.cs
@@ -10,6 +10,15 @@ public class Mod1_Mgr : MonoBehaviour
     public List<GameObject> Puzzle1 = new List<GameObject>();
     public List<GameObject> Puzzle2 = new List<GameObject>();
     public List<GameObject> Puzzle3 = new List<GameObject>();
+
+    public List<Image> Img = new List<Image>();
+    public List<Image> SelPiece = new List<Image>();
+    public List<Image> SetPiece = new List<Image>();
+
+    public List<Image> ImgSprite = new List<Image>();
+    public List<Image> SelPieceSprite = new List<Image>();
+    public List<Image> SetPieceSprite = new List<Image>();
+
     public int Clear_Cnt = 0;
     public int stage = 1;
     public bool[] start = new bool[3] { false, false, false };
@@ -51,16 +60,8 @@ public class Mod1_Mgr : MonoBehaviour
         if (start[0] == false)
         {
             start[0] = true;
-            Puzzle1[0].transform.DOLocalMove(new Vector3(0, -425, 0), 1f).SetEase(Ease.Linear);
-            Puzzle1[1].transform.DOLocalMove(new Vector3(-500, -425, 0), 1f).SetEase(Ease.Linear);
-            Puzzle1[2].transform.DOLocalMove(new Vector3(500, -425, 0), 1f).SetEase(Ease.Linear);
-            //Puzzle1[0].transform.DOMove(new Vector3(960, 115, 0), 1f).SetEase(Ease.Linear);
-            //Puzzle1[1].transform.DOMove(new Vector3(460, 115, 0), 1f).SetEase(Ease.Linear);
-            //Puzzle1[2].transform.DOMove(new Vector3(1460, 115, 0), 1f).SetEase(Ease.Linear);
 
-            Puzzle2[0].transform.DOLocalMove(new Vector3(-500, 100, 0), 1f).SetEase(Ease.Linear);
-            Puzzle2[1].transform.DOLocalMove(new Vector3(0, 100, 0), 1f).SetEase(Ease.Linear);
-            Puzzle2[2].transform.DOLocalMove(new Vector3(500, 100, 0), 1f).SetEase(Ease.Linear);
+            SetSetting();
         }
     }
 
@@ -70,28 +71,7 @@ public class Mod1_Mgr : MonoBehaviour
         {
             start[1] = true;
 
-            Puzzle1[0].SetActive(true);
-            Puzzle1[1].SetActive(true);
-            Puzzle1[2].SetActive(true);
-
-            Puzzle1[0].transform.localScale = new Vector3(2, 2, 2);
-            Puzzle1[1].transform.localScale = new Vector3(2, 2, 2);
-            Puzzle1[2].transform.localScale = new Vector3(2, 2, 2);
-
-            Puzzle1[0].transform.localPosition = new Vector3(2000, -425, 0);
-            Puzzle1[1].transform.localPosition = new Vector3(1500, -425, 0);
-            Puzzle1[2].transform.localPosition = new Vector3(2500, -425, 0);
-
-            Puzzle2[0].transform.localPosition = new Vector3(-1500, 100, 0);
-            Puzzle2[1].transform.localPosition = new Vector3(0, 1000, 0);
-            Puzzle2[2].transform.localPosition = new Vector3(1500, 100, 0);
-
-            Puzzle1[0].transform.DOLocalMove(new Vector3(0, -425, 0), 1f).SetEase(Ease.Linear);
-            Puzzle1[1].transform.DOLocalMove(new Vector3(-500, -425, 0), 1f).SetEase(Ease.Linear);
-            Puzzle1[2].transform.DOLocalMove(new Vector3(500, -425, 0), 1f).SetEase(Ease.Linear);
-            Puzzle2[0].transform.DOLocalMove(new Vector3(-500, 100, 0), 1f).SetEase(Ease.Linear);
-            Puzzle2[1].transform.DOLocalMove(new Vector3(0, 100, 0), 1f).SetEase(Ease.Linear);
-            Puzzle2[2].transform.DOLocalMove(new Vector3(500, 100, 0), 1f).SetEase(Ease.Linear);
+            SetSetting();
         }
     }
 
@@ -101,30 +81,37 @@ public class Mod1_Mgr : MonoBehaviour
         {
             start[2] = true;
 
-            Puzzle1[0].SetActive(true);
-            Puzzle1[1].SetActive(true);
-            Puzzle1[2].SetActive(true);
-
-            Puzzle1[0].transform.localScale = new Vector3(2, 2, 2);
-            Puzzle1[1].transform.localScale = new Vector3(2, 2, 2);
-            Puzzle1[2].transform.localScale = new Vector3(2, 2, 2);
-
-            Puzzle1[0].transform.localPosition = new Vector3(2000, -425, 0);
-            Puzzle1[1].transform.localPosition = new Vector3(1500, -425, 0);
-            Puzzle1[2].transform.localPosition = new Vector3(2500, -425, 0);
-
-            Puzzle2[0].transform.localPosition = new Vector3(-1500, 100, 0);
-            Puzzle2[1].transform.localPosition = new Vector3(0, 1000, 0);
-            Puzzle2[2].transform.localPosition = new Vector3(1500, 100, 0);
-
-            Puzzle1[0].transform.DOLocalMove(new Vector3(0, -425, 0), 1f).SetEase(Ease.Linear);
-            Puzzle1[1].transform.DOLocalMove(new Vector3(-500, -425, 0), 1f).SetEase(Ease.Linear);
-            Puzzle1[2].transform.DOLocalMove(new Vector3(500, -425, 0), 1f).SetEase(Ease.Linear);
-            Puzzle2[0].transform.DOLocalMove(new Vector3(-500, 100, 0), 1f).SetEase(Ease.Linear);
-            Puzzle2[1].transform.DOLocalMove(new Vector3(0, 100, 0), 1f).SetEase(Ease.Linear);
-            Puzzle2[2].transform.DOLocalMove(new Vector3(500, 100, 0), 1f).SetEase(Ease.Linear);
+            SetSetting();
         }
     }
 
+    private void SetSetting()
+    {
+        Puzzle1[0].SetActive(true);
+        Puzzle1[1].SetActive(true);
+        Puzzle1[2].SetActive(true);
 
+        Puzzle3[0].SetActive(true);
+        Puzzle3[1].SetActive(true);
+        Puzzle3[2].SetActive(true);
+
+        Puzzle1[0].transform.localScale = new Vector3(2, 2, 2);
+        Puzzle1[1].transform.localScale = new Vector3(2, 2, 2);
+        Puzzle1[2].transform.localScale = new Vector3(2, 2, 2);
+
+        Puzzle1[0].transform.localPosition = new Vector3(2000, -425, 0);
+        Puzzle1[1].transform.localPosition = new Vector3(1500, -425, 0);
+        Puzzle1[2].transform.localPosition = new Vector3(2500, -425, 0);
+
+        Puzzle2[0].transform.localPosition = new Vector3(-1500, 100, 0);
+        Puzzle2[1].transform.localPosition = new Vector3(0, 1000, 0);
+        Puzzle2[2].transform.localPosition = new Vector3(1500, 100, 0);
+
+        Puzzle1[0].transform.DOLocalMove(new Vector3(0, -425, 0), 1f).SetEase(Ease.Linear);
+        Puzzle1[1].transform.DOLocalMove(new Vector3(-500, -425, 0), 1f).SetEase(Ease.Linear);
+        Puzzle1[2].transform.DOLocalMove(new Vector3(500, -425, 0), 1f).SetEase(Ease.Linear);
+        Puzzle2[0].transform.DOLocalMove(new Vector3(-500, 100, 0), 1f).SetEase(Ease.Linear);
+        Puzzle2[1].transform.DOLocalMove(new Vector3(0, 100, 0), 1f).SetEase(Ease.Linear);
+        Puzzle2[2].transform.DOLocalMove(new Vector3(500, 100, 0), 1f).SetEase(Ease.Linear);
+    }
 }
